be296dabc586e4338be0ded590e7d508076a426d
tcloutier4 <thomas.cloutier.r@gmail.com>
2021-08-06 12:25:57 -0700

    Set up framework of all views and all controllers

---
+++ CourseProject/Controller/GroceryListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GroceryListViewController.swift
+//  CourseProject
+//
+//  Created by user196933 on 8/6/21.
+//
+
+import UIKit
+
+class GroceryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
